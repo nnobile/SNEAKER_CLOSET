ef20a0c3a57aad2067e1ec29fc1c53ba92f1a08b
Nick Nobile <nickcnobile@gmail.com>
2020-12-15 13:09:27 -0800

    Slight syntax adjustment to User migration file

--- db/migrate/20201215204352_create_users.rb
+++ db/migrate/20201215204352_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |u|
-      u.string :name
-      u.string :email
-      u.string :password_digest
-      u.timestamp
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.timestamp
     end
   end
 end

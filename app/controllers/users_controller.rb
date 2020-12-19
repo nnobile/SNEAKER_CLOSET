3acee11937bc5a707624cae5f312bf403e67a348
Nick Nobile <nickcnobile@gmail.com>
2020-12-19 09:11:26 -0800

    edit typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     post "/signup" do
         user = User.new(:email => params[:email], :password => params[:password])
         if user.save
-            sessions[:user_id] = user.id
+            session[:user_id] = user.id
             redirect "/sneakers"
           else
             redirect "/failure"

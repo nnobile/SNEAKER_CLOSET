2c250a8ccd738d407a6fd85e9ca4e687bf90836e
Nick Nobile <nickcnobile@gmail.com>
2020-12-19 09:43:19 -0800

    build logout route in user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     post "/signup" do
-        user = User.new(:email => params[:email], :password => params[:password])
+        @user = User.new(:email => params[:email], :password => params[:password])
         if user.save
             session[:user_id] = user.id
             redirect "/sneakers"
@@ -15,13 +15,18 @@ class UsersController < ApplicationController
     end
     
     post "/login" do
-        user = User.find_by(:email => params[:email])
+        @user = User.find_by(:email => params[:email])
         if user && user.authenticate(params[:password])
           session[:user_id] = user.id
           redirect "/sneakers"
         else
           redirect "/failure"
         end
+    end
+
+    get "/logout" do
+        session[:user_id] = nil
+        redirect to "/loggedout"
     end
 
 end

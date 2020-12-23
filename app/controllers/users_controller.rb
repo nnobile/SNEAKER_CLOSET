6e0534c15e92016a577d591c7f60e17f59e5e7ef
Nick Nobile <nickcnobile@gmail.com>
2020-12-23 12:50:52 -0800

    slight update in user signup action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     post '/signup' do
-        @user = User.new(params[:user])
+        user = User.new(params[:user])
         if user.save
             session[:user_id] = user.id
             redirect '/sneakers'

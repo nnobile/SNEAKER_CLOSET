fddfad9b1c5b7690f5be2d2f120d75abc3a0b6db
Nick Nobile <nickcnobile@gmail.com>
2020-12-22 18:18:18 -0800

    updated schema to remove email and updated related files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
 
     post '/signup' do
-        @user = User.new(:email => params[:email], :password => params[:password])
+        @user = User.new(:username => params[:username], :password => params[:password])
         if user.save
             session[:user_id] = user.id
             redirect '/sneakers'
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     end
     
     post '/login' do
-        @user = User.find_by(:email => params[:email])
+        @user = User.find_by(:username => params[:username])
         if user && user.authenticate(params[:password])
           session[:user_id] = user.id
           redirect '/sneakers'

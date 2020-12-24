cac5fa822c8cd48794212d972fa202544ed34359
Nick Nobile <nickcnobile@gmail.com>
2020-12-24 09:36:44 -0800

    changed user to local variable in login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,8 @@ class UsersController < ApplicationController
     end
     
     post '/login' do
-        @user = User.find_by(:username => params[:username])
+        user = User.find_by(:username => params[:username])
+        binding.pry
         if @user && @user.authenticate(params[:password])
           session[:user_id] = @user.id
           redirect '/sneakers'

f54ffa6ae99045204fb517b87ad5077ea8620584
Nick Nobile <nickcnobile@gmail.com>
2020-12-28 14:46:52 -0800

    revise welcome route in applcation controller with new logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :welcome
+    if logged_in?
+      redirect to '/sneakers'
+    else
+      erb :welcome
+    end
   end
 
   helpers do 
